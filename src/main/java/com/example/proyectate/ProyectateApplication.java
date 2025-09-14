c8c00f47297255c3e480f587df0652ac43feb8da
XxemicoxX <emiliojc2004@gmail.com>
2025-09-13 20:03:36 -0500

    Creacion del proyecto

---
+++ src/main/java/com/example/proyectate/ProyectateApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectate/ProyectateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
